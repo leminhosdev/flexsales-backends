019e8433a0db9a9abb181e49944e3fb1b4faad59
leminhosdev <lucaslemos8294@gmail.com>
2023-07-09 15:42:05 -0300

    first commit

---
+++ src/main/java/com/example/flexsaless/FlexsalessApplication.java
@@ -0,0 +1,13 @@
+package com.example.flexsaless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexsalessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexsalessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flexsaless/FlexsalessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flexsaless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexsalessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
