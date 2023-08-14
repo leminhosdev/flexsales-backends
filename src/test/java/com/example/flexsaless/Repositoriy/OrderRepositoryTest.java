e18c7c167bc77699ca89be3ed27981903a2bfb45
leminhosdev <lucaslemos8294@gmail.com>
2023-08-14 15:40:43 -0300

    TESTS: All repositorys tests made

--- src/test/java/com/example/flexsaless/Repositoriy/OrderRepositoryTest.java
+++ src/test/java/com/example/flexsaless/Repositoriy/OrderRepositoryTest.java
@@ -1,13 +1,85 @@
 package com.example.flexsaless.Repositoriy;
 
+import com.example.flexsaless.Entitiy.Client;
+import com.example.flexsaless.Entitiy.OrderEntity;
+import com.example.flexsaless.Entitiy.Product;
 import com.example.flexsaless.Repository.ClientRepository;
 import com.example.flexsaless.Repository.OrderRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 public class OrderRepositoryTest {
     @Autowired
-    private OrderRepository clientRepository;
+    private OrderRepository orderRepository;
 
-}
+
+    @Test
+    void save_PersistClient_WhenSucessful(){
+        OrderEntity orderToBeSaved = this.createOrder();
+        OrderEntity orderSaved =  this.orderRepository.save(orderToBeSaved);
+        Assertions.assertNotNull(orderSaved);
+        Assertions.assertNotNull(orderSaved.getId());
+        Assertions.assertNotNull(orderSaved.getClientOrderOwner());
+        Assertions.assertFalse(orderSaved.getProductList().isEmpty());
+        Assertions.assertEquals(orderSaved, orderToBeSaved);
+    }
+    @Test
+    void update_UpdateClient_WhenSucessful(){
+        OrderEntity orderToBeSaved = this.createOrder();
+        OrderEntity orderSaved =  this.orderRepository.save(orderToBeSaved);
+        orderSaved.setProductsAmount(299);
+        OrderEntity orderUpdated =  this.orderRepository.save(orderSaved);
+
+        Assertions.assertNotNull(orderSaved);
+        Assertions.assertNotNull(orderSaved.getId());
+        Assertions.assertNotNull(orderSaved.getClientOrderOwner());
+        Assertions.assertFalse(orderSaved.getProductList().isEmpty());
+        Assertions.assertEquals(orderSaved, orderToBeSaved);
+        Assertions.assertNotNull(orderUpdated);
+        Assertions.assertNotNull(orderUpdated.getId());
+
+    }
+    @Test
+    void delete_DeleteClient_WhenSucessful(){
+        OrderEntity orderToBeSaved = this.createOrder();
+        OrderEntity orderSaved =  this.orderRepository.save(orderToBeSaved);
+        this.orderRepository.delete(orderSaved);
+        Optional<OrderEntity> productOptional = this.orderRepository.findById(orderSaved.getId());
+        Assertions.assertTrue(productOptional.isEmpty());
+
+    }
+
+
+    private OrderEntity createOrder() {
+        Product product = createProduct();
+        List<Product> productList = new ArrayList<Product>();
+        productList.add(product);
+        LocalDate date = LocalDate.parse("2023-10-10");
+        return OrderEntity.builder().clientOrderOwner(product.getClientOwner())
+                .productList(productList).productsAmount(10)
+                .salesData(date).totalPrice(new BigDecimal(200))
+                .totalTaxesValue(new BigDecimal(200)).totalCommissionValue(new BigDecimal(200)).build();
+
+    }
+    public Product createProduct(){
+
+        Client clientOwner = Client.builder().clientName("Peoples").clientArea("Business").email("ada3ff@gmail.com")
+                .password("topasdm3das").build();
+
+        return   Product.builder().price(new BigDecimal(200)).code("adasd3d")
+                .name("ropeiro dois 2")
+                .commission(new BigDecimal(10)).taxes(new BigDecimal(2))
+                .clientOwner(clientOwner).build();
+    }
+
+    }
--- src/test/java/com/example/flexsaless/Repositoriy/ProductRepositoryTest.java
+++ src/test/java/com/example/flexsaless/Repositoriy/ProductRepositoryTest.java
@@ -1,4 +1,57 @@
 package com.example.flexsaless.Repositoriy;
 
+import com.example.flexsaless.Entitiy.Client;
+import com.example.flexsaless.Entitiy.Product;
+import com.example.flexsaless.Repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@DataJpaTest
 public class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+    @Test
+    void save_PersistClient_WhenSucessful(){
+        Product productTobeSaved = this.createProduct();
+        Product productSaved =  this.productRepository.save(productTobeSaved);
+        Assertions.assertNotNull(productSaved);
+        Assertions.assertNotNull(productSaved.getId());
+        Assertions.assertEquals(productSaved, productTobeSaved);
+    }
+    @Test
+    void update_UpdateClient_WhenSucessful(){
+        Product productTobeSaved = this.createProduct();
+        Product productSaved =  this.productRepository.save(productTobeSaved);
+        productSaved.setName("see this name");
+        Product productUpdated =  this.productRepository.save(productSaved);
+
+        Assertions.assertNotNull(productUpdated);
+        Assertions.assertNotNull(productUpdated.getId());
+        Assertions.assertEquals(productSaved, productTobeSaved);
+    }
+    @Test
+    void delete_DeleteClient_WhenSucessful(){
+        Product productTobeSaved = this.createProduct();
+        Product productSaved =  this.productRepository.save(productTobeSaved);
+        this.productRepository.delete(productSaved);
+        Optional<Product> productOptional = this.productRepository.findById(productSaved.getId());
+        Assertions.assertTrue(productOptional.isEmpty());
+
+    }
+
+    private Product createProduct(){
+
+        Client clientOwner = Client.builder().clientName("Peoples").clientArea("Business").email("ada3ff@gmail.com")
+                .password("topasdm3das").build();
+
+      return   Product.builder().price(new BigDecimal(200)).code("adasd3d")
+                .name("ropeiro dois 2")
+                .commission(new BigDecimal(10)).taxes(new BigDecimal(2))
+                .clientOwner(clientOwner).build();
+    }
 }
